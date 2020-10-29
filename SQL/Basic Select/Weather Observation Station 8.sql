db68789f285c4b1b84895affccb1930a82102932
Star-Asia S. Armand <66345791+Belle-Star@users.noreply.github.com>
2020-10-29 07:29:19 -0400

    Update Weather Observation Station 8.sql

--- SQL/Basic Select/Weather Observation Station 8.sql
+++ SQL/Basic Select/Weather Observation Station 8.sql
@@ -2,3 +2,7 @@
 
 SELECT DISTINCT CITY FROM STATION
 WHERE CITY REGEXP '^[aeiou].*[aeiou]$';
+
+-- this would be the correct changes, the solution ^ doesn't work with oracle. techinally it was close but they wanted the answer to be more detailed --
+SELECT DISTINCT CITY FROM STATION WHERE REGEXP_LIKE(LOWER(CITY), '^[aeiou]') intersect
+SELECT DISTINCT CITY FROM STATION WHERE REGEXP_LIKE(LOWER(CITY), '[aeiou]$');
